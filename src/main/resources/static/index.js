4a48f726ef54230fc90911b1bf4406b0ae2772ba
Abrazacs <sergeysemenov87@gmail.com>
2022-04-04 01:57:10 +0300

    Done

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,27 +1,27 @@
 angular.module('market', []).controller('indexController', function ($scope, $http) {
     $scope.fillTable = function () {
-        $http.get('http://localhost:8189/market/api/v1/products')
+        $http.get('http://localhost:8180/market/api/v1/products')
             .then(function (response) {
                 $scope.products = response.data;
-                console.log(response);
             });
-        $http.get('http://localhost:8189/market/api/v1/cart')
+    }
+
+    $scope.fillCart = function (){
+        $http.get('http://localhost:8180/market/api/v1/products/cart')
             .then(function (response){
                 $scope.productsInCart = response.data;
-                console.log(response);
             });
     }
 
     $scope.deleteProduct = function (id) {
-        $http.delete('http://localhost:8189/market/api/v1/products/' + id)
+        $http.delete('http://localhost:8180/market/api/v1/products/' + id)
             .then(function (response) {
                 $scope.fillTable();
             });
     }
 
     $scope.createNewProduct = function () {
-        // console.log($scope.newProduct);
-        $http.post('http://localhost:8189/market/api/v1/products', $scope.newProduct)
+        $http.post('http://localhost:8180/market/api/v1/products', $scope.newProduct)
             .then(function (response) {
                 $scope.newProduct = null;
                 $scope.fillTable();
@@ -29,11 +29,12 @@ angular.module('market', []).controller('indexController', function ($scope, $ht
     }
 
     $scope.addProduct = function (id){
-        $http.get('http://localhost:8189/market/api/v1/cart/'+id)
+        $http.get('http://localhost:8180/market/api/v1/products/cart/'+id)
             .then(function (response){
-                $scope.fillTable();
+                $scope.fillCart();
             });
     }
 
     $scope.fillTable();
+    $scope.fillCart();
 })
